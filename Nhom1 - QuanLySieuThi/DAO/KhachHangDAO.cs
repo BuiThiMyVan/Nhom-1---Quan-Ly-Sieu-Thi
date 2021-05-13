a7d9a3e801919ca771b245b50a004c48ebe74455
longkieuck <longkieuck@gmail.com>
2021-05-13 22:43:04 +0700

    add KhachHangDao and Procedure for KhachHang

---
+++ Nhom1 - QuanLySieuThi/DAO/KhachHangDAO.cs
@@ -0,0 +1,60 @@
+﻿using Nhom1___QuanLySieuThi.Models;
+using NHOM1_QLKHO.DatAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhom1___QuanLySieuThi.DAO
+{
+    class KhachHangDAO
+    {
+        private static KhachHangDAO instance;
+
+        internal static KhachHangDAO Instance
+        {
+            get { if (instance == null) instance = new KhachHangDAO(); return instance; }
+            private set { instance = value; }
+        }
+        public List<KhachHang> GetAll()
+        {
+            List<KhachHang> list = new List<KhachHang>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SP_KhachHang_GetAll");
+            foreach (DataRow item in data.Rows)
+            {
+                KhachHang entry = new KhachHang(item);
+                list.Add(entry);
+            }
+            return list;
+        }
+        public bool Insert(string tenKhachHang, string diaChi, string soDienThoai)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_KhachHang_Insert @tenKhachHang , @diaChi , @soDienThoai", new object[] { tenKhachHang, diaChi, soDienThoai});
+            return result > 0;
+        }
+        public bool Update(int maKH, string tenKhachHang, string diaChi, string soDienThoai)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_KhachHang_Update @maKH , @tenKhachHang , @diaChi , @soDienThoai", new object[] { maKH, tenKhachHang, diaChi, soDienThoai });
+            return result > 0;
+        }
+        public bool Delete(int idKhachHang)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_KhachHang_Delete @idKhachHang", new object[] { idKhachHang });
+
+            return result > 0;
+        }
+        public List<KhachHang> Search(string searchValue)
+        {
+            List<KhachHang> list = new List<KhachHang>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SP_KhachHang_Search @searchValue", new object[] { searchValue });
+            foreach (DataRow item in data.Rows)
+            {
+                KhachHang entry = new KhachHang(item);
+                list.Add(entry);
+            }
+            return list;
+        }
+    }
+}
--- Nhom1 - QuanLySieuThi/Database/StoreProcedure.sql
+++ Nhom1 - QuanLySieuThi/Database/StoreProcedure.sql
@@ -1,2 +1,65 @@
 USE NHOM1_QUANLYSIEUTHI
 
+-------------------------------------------------KDLONG-----------------------------------------------------------
+CREATE PROCEDURE SP_KhachHang_GetAll
+AS
+BEGIN
+  SELECT *
+  FROM KhachHang
+END
+GO
+
+CREATE PROCEDURE SP_KhachHang_Insert
+  @tenKhachHang NVARCHAR(50),
+  @diaChi NVARCHAR(100),
+  @soDienThoai NVARCHAR(10)
+AS
+BEGIN
+  INSERT INTO KHACHHANG
+    (TenKH, DiaChi, SDT)
+  VALUES(@tenKhachHang, @diaChi, @soDienThoai)
+END
+GO
+
+CREATE PROCEDURE SP_KhachHang_Delete
+  @maKH INT
+AS
+BEGIN
+  UPDATE HOADONBAN
+  SET MaKH = NULL
+  WHERE MaKH = @maKH
+
+  DELETE KHACHHANG
+  WHERE MaKH = @maKH
+END
+GO
+
+CREATE PROCEDURE SP_KhachHang_Update
+  @maKH INT,
+  @tenKhachHang NVARCHAR(50),
+  @diaChi NVARCHAR(100),
+  @soDienThoai NVARCHAR(10)
+AS
+BEGIN
+  UPDATE KhachHang
+  SET 
+  TenKH = @tenKhachHang,
+  DiaChi = @diaChi,
+  SDT = @soDienThoai
+  WHERE MaKH = @maKH
+END
+GO
+
+CREATE PROCEDURE SP_KhachHang_Search
+  @searchValue NVARCHAR(200)
+AS
+BEGIN
+  SELECT *
+  FROM KhachHang
+  WHERE MaKH LIKE N'%' + @searchValue + '%'
+    OR TenKH LIKE N'%' + @searchValue + '%'
+    OR DiaChi LIKE N'%' + @searchValue + '%'
+    OR SDT LIKE N'%' + @searchValue + '%'
+END
+GO
+-------------------------------------------------------/KDLONG---------------------------------------------
